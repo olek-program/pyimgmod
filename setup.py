5fbc8345b79b794239f5df018431cfbd07f47d91
Olek <94283587+olek-program@users.noreply.github.com>
2023-02-26 10:52:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 from setuptools import setup, find_packages
-
-VERSION = '0.0.01'
-DESCRIPTION = 'Generating random imagaes and manipulating it'
-LONG_DESCRIPTION = 'Library for python with this library you can generate random images and modify it'
-
+ 
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
 setup(
-    name="pyimgen",
-    version=VERSION,
-    author="Lemskyy (olek-program)",
-    author_email="<lemskyyyt@gmail.com>",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=['pypng'],
-    keywords=['python', 'pypng', 'png', 'images'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+  name='genimg',
+  version='0.0.1',
+  description='Library for generating  images and modify it',
+  long_description='This library can generate imaages and can modify it this is version in programming',
+  url='',  
+  author='Lemskyy',
+  author_email='lemskyyyt@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='python images generator modyfier',
+  packages=find_packages(),
+  install_requires=['pypng'] 
 )
